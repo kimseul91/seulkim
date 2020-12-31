c4f737c3162c7553b137d9d30d46b939f624b878
kimseul91 <59694254+kimseul91@users.noreply.github.com>
2020-12-30 20:58:17 -0500

    added CKEditor 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 import Land from "./components/Land/Land"
 import Blogs from "./components/Blog/Blogs"
--- src/components/Blog/Blogs.js
+++ src/components/Blog/Blogs.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import "../Blog/Blogs.css";
-// import "../page.css";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import "./Blogs.css";
 
 
 const Blogs = () => {
+    const [content, setContent] = useState("");
     const [innerHeight, setInnerHeight] = useState(window.innerHeight);
     
     useEffect(() => {
@@ -14,16 +16,36 @@ const Blogs = () => {
         window.addEventListener('resize',handleResized);
     })
     return (
-        <div className="container">
-            <div id="home" className="page name blog-pending" style={{height: innerHeight}}>
-                <div className="pending">
-
-                    <h1> Blogs </h1>
-                    <h2> Currently in the process of making.</h2>
-                    <a href="/">Go back Home</a>
-                </div>
+        <div className="blog-container" style={{height: innerHeight}}>
+            <div id="editor">
+                <h3>Hello Working on Blogging</h3>
             </div>
+            <div>
+                <p dangerouslySetInnerHTML= {{ __html:  content}} />
+            </div>
+            <div className="CKEditor-input">
+                <CKEditor
+                        editor={ ClassicEditor }
+                        data="<p>Hello from CKEditor 5!</p>"
+                        onReady={ editor => {
+                            // You can store the "editor" and use when it is needed.
+                            // console.log( 'Editor is ready to use!', editor );
+                        } }
+                        onChange={ ( event, editor ) => {
+                            const data = editor.getData();
+                            // console.log( { event, editor, data } );
+                            setContent(data);
+                            console.log(content);
 
+                        } }
+                        onBlur={ ( event, editor ) => {
+                            // console.log( 'Blur.', editor );
+                        } }
+                        onFocus={ ( event, editor ) => {
+                            // console.log( 'Focus.', editor );
+                        } }
+                    />
+            </div>
         </div>
     );
 };
